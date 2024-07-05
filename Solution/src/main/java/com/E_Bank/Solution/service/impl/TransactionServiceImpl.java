5bd96fa0a52f33a2c19eac52a3bd450c446f37ff
ilyashq <ilyasenaa24@gmail.com>
2024-07-05 09:58:33 +0100

    des modifications dans TransactionServiceImpl

--- Solution/src/main/java/com/E_Bank/Solution/service/impl/TransactionServiceImpl.java
+++ Solution/src/main/java/com/E_Bank/Solution/service/impl/TransactionServiceImpl.java
@@ -1,4 +1,43 @@
 package com.E_Bank.Solution.service.impl;
 
-public class TransactionServiceImpl {
+import com.E_Bank.Solution.dto.TransactionDTO;
+import com.E_Bank.Solution.mapper.TransactionMapper;
+import com.E_Bank.Solution.model.Transaction;
+import com.E_Bank.Solution.repository.TransactionRepository;
+import com.E_Bank.Solution.service.TransactionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class TransactionServiceImpl implements TransactionService {
+
+    @Autowired
+    private TransactionRepository transactionRepository;
+
+    @Autowired
+    private TransactionMapper transactionMapper;
+
+    @Override
+    public TransactionDTO createTransaction(TransactionDTO transactionDTO) {
+        Transaction transaction = transactionMapper.toEntity(transactionDTO);
+        transaction = transactionRepository.save(transaction);
+        return transactionMapper.toDTO(transaction);
+    }
+
+    @Override
+    public List<TransactionDTO> getAllTransactions() {
+        return transactionRepository.findAll().stream()
+                .map(transactionMapper::toDTO)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public TransactionDTO getTransactionById(Long id) {
+        Transaction transaction = transactionRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Transaction not found"));
+        return transactionMapper.toDTO(transaction);
+    }
 }
