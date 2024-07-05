95997c075023f335da2b197c19e1391c59e08b62
ilyashq <ilyasenaa24@gmail.com>
2024-07-05 10:11:46 +0100

    création des services et implémentations pour Users

--- Solution/src/main/java/com/E_Bank/Solution/service/UserService.java
+++ Solution/src/main/java/com/E_Bank/Solution/service/UserService.java
@@ -5,8 +5,8 @@ import com.E_Bank.Solution.dto.UserDTO;
 import java.util.List;
 
 public interface UserService {
-    UserDTO createUser(UserDTO userDTO);
-    List<UserDTO> getAllUsers();
+//    UserDTO createUser(UserDTO userDTO);
+//    List<UserDTO> getAllUsers();
     UserDTO getUserById(Long id);
     UserDTO updateUser(Long id, UserDTO userDTO);
     void deleteUser(Long id);
--- dockertest/src/main/java/com/example/dockertest/helloController.java
+++
@@ -1,2 +0,0 @@
-package com.example.dockertest;public class helloController {
-}
